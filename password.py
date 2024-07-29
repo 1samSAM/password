619a48faaedb831f9932e5720912514c52d3b678
project 1 <samourhome7@gmail.com>
2024-07-29 23:46:55 +0530

    add cryptograph

--- password.py
+++ password.py
@@ -1,11 +1,27 @@
+from cryptography.fernet import Fernet
+
+
+def load_key():
+    file = open("key.key","rb")
+    key = file.read()
+    file.close()
+    return key
+
+
 master_pwd = input("what is the master password ?")
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key","wb") as key_file:
+        key_file.write(key)
 
 def view():
     with open("password.txt","r") as f:
         for line in f .readlines():
             data = line
             user,passw = data.split("|")
-            print("user:",user,"password:",passw)
+            print("user:",user,"password:",str(fer.decrypt(passw.encode())))
 
 
 
@@ -13,7 +29,7 @@ def add():
     name = input ("account name :")
     pwd = input ("password :")
     with open("password.txt","a") as f: 
-        f.write(name +"|" + pwd + "\n")
+        f.write(name +"|" + str(fer.encrypt(pwd.encode())) + "\n")
 
     
 
