315b368a408a2be7d8868155b8dfe1aad56f4c36
project 1 <samourhome7@gmail.com>
2024-07-31 01:11:42 +0530

    adding changes

--- password.py
+++ password.py
@@ -1,42 +1,43 @@
 from cryptography.fernet import Fernet
 
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)'''
+
 
 def load_key():
-    file = open("key.key","rb")
+    file = open("key.key", "rb")
     key = file.read()
     file.close()
     return key
 
 
-master_pwd = input("what is the master password ?")
-key = load_key() + master_pwd.encode()
+key = load_key()
 fer = Fernet(key)
-def write_key():
-    key = Fernet.generate_key()
-    with open("key.key","wb") as key_file:
-        key_file.write(key)
+
 
 def view():
-    with open("password.txt","r") as f:
-        for line in f .readlines():
-            data = line
-            user,passw = data.split("|")
-            print("user:",user,"password:",str(fer.decrypt(passw.encode())))
-
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User:", user, "| Password:",
+                  fer.decrypt(passw.encode()).decode())
 
 
 def add():
-    name = input ("account name :")
-    pwd = input ("password :")
-    with open("password.txt","a") as f: 
-        f.write(name +"|" + str(fer.encrypt(pwd.encode())) + "\n")
+    name = input('Account Name: ')
+    pwd = input("Password: ")
 
-    
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+
 
 while True:
-
-
-    mode = input("would you like to add a new password or view existing one (view, add)or (q) ?").lower()
+    mode = input(
+        "Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
     if mode == "q":
         break
 
@@ -44,7 +45,6 @@ while True:
         view()
     elif mode == "add":
         add()
-
     else:
-        print("invalid mode")
-
+        print("Invalid mode.")
+        continue
